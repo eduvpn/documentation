129593a50c501cbfd1d12067ad0430c514298457
François Kooman <fkooman@tuxed.net>
2014-10-26 23:26:05 +0100

    comment the sign and rsync lines

--- rpm/build_all.sh
+++ rpm/build_all.sh
@@ -7,10 +7,10 @@ rpmdev-wipetree
 sh build.sh vpn-cert-service fkooman vpn-cert-service 0.1.11
 sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.1.11
 
-rpm --resign ${HOME}/rpmbuild/RPMS/noarch/*.rpm
+#rpm --resign ${HOME}/rpmbuild/RPMS/noarch/*.rpm
 
 # create a yum repo
 createrepo ${HOME}/rpmbuild/RPMS/
 
 # rsync to a web server
-rsync --delete --recursive ${HOME}/rpmbuild/RPMS fkooman@ursa.uberspace.de:/var/www/virtual/fkooman/www.php-oauth.net/repo/eduVPN
+#rsync --recursive ${HOME}/rpmbuild/RPMS fkooman@ursa.uberspace.de:/var/www/virtual/fkooman/www.php-oauth.net/repo/eduVPN
