6d44eee8593cc9af2b04e49b3851ae2fd084e06a
François Kooman <fkooman@tuxed.net>
2014-11-18 13:59:40 +0100

    also build vpn-crl-fetcher

--- rpm/build_all.sh
+++ rpm/build_all.sh
@@ -6,11 +6,11 @@ rpmdev-wipetree
 # build all projects
 sh build.sh vpn-cert-service fkooman vpn-cert-service 0.1.13
 sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.1.11
-
-#rpm --resign ${HOME}/rpmbuild/RPMS/noarch/*.rpm
+sh build.sh vpn-crl-fetcher  fkooman vpn-crl-fetcher  0.1.1
+rpm --resign ${HOME}/rpmbuild/RPMS/noarch/*.rpm
 
 # create a yum repo
 createrepo ${HOME}/rpmbuild/RPMS/
 
 # rsync to a web server
-#rsync --recursive ${HOME}/rpmbuild/RPMS fkooman@ursa.uberspace.de:/var/www/virtual/fkooman/www.php-oauth.net/repo/eduVPN
+rsync --recursive ${HOME}/rpmbuild/RPMS fkooman@ursa.uberspace.de:/var/www/virtual/fkooman/www.php-oauth.net/repo/eduVPN
