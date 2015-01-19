afbdda8b3f97415373629a22aeb575d10176d6d9
François Kooman <fkooman@tuxed.net>
2015-01-19 23:07:27 +0100

    fix docker server config generation, new version of user portal

--- rpm/build_all.sh
+++ rpm/build_all.sh
@@ -5,5 +5,5 @@ rpmdev-wipetree
 
 # build all projects
 sh build.sh vpn-cert-service fkooman vpn-cert-service 0.1.18
-sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.2.1
+sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.2.2
 sh build.sh vpn-crl-fetcher  fkooman vpn-crl-fetcher  0.1.1
