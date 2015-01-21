4d9290abef095ce4ce6b625a899b2fcee66a8380
François Kooman <fkooman@tuxed.net>
2015-01-21 12:15:46 +0100

    update rpm versions, update Dockerfile

--- rpm/build_all.sh
+++ rpm/build_all.sh
@@ -4,6 +4,6 @@
 rpmdev-wipetree
 
 # build all projects
-sh build.sh vpn-cert-service fkooman vpn-cert-service 0.1.18
-sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.2.2
+sh build.sh vpn-cert-service fkooman vpn-cert-service 0.1.20
+sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.2.3
 sh build.sh vpn-crl-fetcher  fkooman vpn-crl-fetcher  0.1.1
