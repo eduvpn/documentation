7ddfaa1285ecb0651631813d7fb13d769ae893a4
François Kooman <fkooman@tuxed.net>
2015-03-15 19:47:59 +0100

    update versions

--- rpm/build_all.sh
+++ rpm/build_all.sh
@@ -4,6 +4,6 @@
 rpmdev-wipetree
 
 # build all projects
-sh build.sh vpn-cert-service fkooman vpn-cert-service 0.1.21
-sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.2.8
+sh build.sh vpn-cert-service fkooman vpn-cert-service 0.2.0
+sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.3.0
 sh build.sh vpn-crl-fetcher  fkooman vpn-crl-fetcher  0.1.1
