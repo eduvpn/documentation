1136252594c7ea45e9639e881ba3c939f099c58e
François Kooman <fkooman@tuxed.net>
2015-04-11 18:25:35 +0200

    update build script, update eduVPN branded template

--- rpm/build_all.sh
+++ rpm/build_all.sh
@@ -5,5 +5,5 @@ rpmdev-wipetree
 
 # build all projects
 sh build.sh vpn-cert-service fkooman vpn-cert-service 0.2.0
-sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.3.0
+sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.3.1
 sh build.sh vpn-crl-fetcher  fkooman vpn-crl-fetcher  0.1.1
