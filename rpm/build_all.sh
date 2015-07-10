37e328c73769ba9eab473bf1c7a300e6b3c41c03
François Kooman <fkooman@tuxed.net>
2015-07-10 12:02:59 +0200

    fix MELLON-NAME-ID header handling

--- rpm/build_all.sh
+++ rpm/build_all.sh
@@ -6,7 +6,7 @@ sh build.sh vpn-cert-service fkooman vpn-cert-service 0.3.0
 scp $HOME/rpmbuild/SPECS/*.spec $HOME/rpmbuild/SRPMS/*.rpm fedorapeople.org:~/public_html/vpn-cert-service/
 
 rpmdev-wipetree
-sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.4.1
+sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.4.2
 scp $HOME/rpmbuild/SPECS/*.spec $HOME/rpmbuild/SRPMS/*.rpm fedorapeople.org:~/public_html/vpn-user-portal/
 
 rpmdev-wipetree
