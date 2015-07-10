d69435d7c1ae06cb1aae7345b75b4a7d6267f2ed
François Kooman <fkooman@tuxed.net>
2015-07-10 11:47:20 +0200

    update vpn-crl-fetcher version

--- rpm/build_all.sh
+++ rpm/build_all.sh
@@ -10,5 +10,5 @@ sh build.sh vpn-user-portal  fkooman vpn-user-portal  0.4.1
 scp $HOME/rpmbuild/SPECS/*.spec $HOME/rpmbuild/SRPMS/*.rpm fedorapeople.org:~/public_html/vpn-user-portal/
 
 rpmdev-wipetree
-sh build.sh vpn-crl-fetcher  fkooman vpn-crl-fetcher  0.1.2
+sh build.sh vpn-crl-fetcher  fkooman vpn-crl-fetcher  0.1.3
 scp $HOME/rpmbuild/SPECS/*.spec $HOME/rpmbuild/SRPMS/*.rpm fedorapeople.org:~/public_html/vpn-crl-fetcher/
