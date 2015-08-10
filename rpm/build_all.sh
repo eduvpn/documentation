eee3542a3fd8cf386d813304078f02b1467a2d25
François Kooman <fkooman@tuxed.net>
2015-08-10 16:01:22 +0200

    update component versions

--- rpm/build_all.sh
+++ rpm/build_all.sh
@@ -2,11 +2,11 @@
 
 # build all projects
 rpmdev-wipetree
-sh build.sh vpn-cert-service fkooman vpn-cert-service 1.0.0
+sh build.sh vpn-cert-service fkooman vpn-cert-service 1.0.1
 scp $HOME/rpmbuild/SPECS/*.spec $HOME/rpmbuild/SRPMS/*.rpm fedorapeople.org:~/public_html/vpn-cert-service/
 
 rpmdev-wipetree
-sh build.sh vpn-user-portal  fkooman vpn-user-portal 1.0.0
+sh build.sh vpn-user-portal  fkooman vpn-user-portal 1.0.1
 scp $HOME/rpmbuild/SPECS/*.spec $HOME/rpmbuild/SRPMS/*.rpm fedorapeople.org:~/public_html/vpn-user-portal/
 
 rpmdev-wipetree
