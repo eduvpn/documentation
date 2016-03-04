51892339a0dc76925aa6108eb7e09023262b0c54
François Kooman <fkooman@tuxed.net>
2016-03-04 14:23:30 +0100

    fix comment

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,6 @@
 # - there is a default user:pass for the user interface and admin interface,
 #   foo:bar, which should be updated and printed at the end of the script so
 #   they are unique for every instance!
-# WTF is up with the session directory :(
 
 ###############################################################################
 # VARIABLES
