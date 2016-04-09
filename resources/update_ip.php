daa8428f66026c3ae91a35612dfa7dfbfc046fa7
François Kooman <fkooman@tuxed.net>
2016-04-09 19:34:46 +0200

    add documentation to update_ip.php

--- resources/update_ip.php
+++ resources/update_ip.php
@@ -1,11 +1,22 @@
 <?php
 
+/**
+ * Update the IP address configuration of vpn-server-api.
+ *
+ * IPv4:
+ * Random value for the second and third octet, e.g: 10.53.129.0/24
+ *
+ * IPv6:
+ * The IPv6 address is generated according to RFC 4193 (Global ID), it results 
+ * in a /48 network.
+ *
+ * The addresses are written to /etc/vpn-server-api/ip.yaml
+ */
 require_once '/usr/share/php/fkooman/Config/autoload.php';
 require_once '/usr/share/php/random_compat/autoload.php';
 
 use fkooman\Config\YamlFile;
 
-// IPv4
 $v4 = sprintf('10.%s.%s.0/24', hexdec(bin2hex(random_bytes(1))), hexdec(bin2hex(random_bytes(1))));
 $v6 = sprintf('fd%s:%s:%s::/48', bin2hex(random_bytes(1)), bin2hex(random_bytes(2)), bin2hex(random_bytes(2)));
 
