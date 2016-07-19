aa3b54d351759b7a9720a0fa65c586233876f4a5
François Kooman <fkooman@tuxed.net>
2016-07-19 14:30:29 +0200

    fix disable revoked certs script

--- attic/disable_revoked_certs.php
+++ attic/disable_revoked_certs.php
@@ -9,11 +9,11 @@ use fkooman\VPN\CA\IndexParser;
 $indexPath = '/var/lib/vpn-ca-api/easy-rsa/pki/index.txt'; # EasyRSA3
 #$indexPath = '/var/www/vpn-ca-api/data/easy-rsa/pki/index.txt'; # Development
 
-$commonNamesDir = '/var/lib/vpn-server-api/common_names/disabled';
-#$commonNamesDir = '/var/www/vpn-server-api/data/common_names/disabled';
+$disabledDir = '/var/lib/vpn-server-api/common_names/disabled';
+#$disabledDir = '/var/www/vpn-server-api/data/common_names/disabled';
 
 // create dir if it does not yet exists
-@mkdir($commonNamesDir, 0751, true);
+@mkdir($disabledDir, 0751, true);
 
 // find the revoked CNs 
 $i = new IndexParser($indexPath);
@@ -23,12 +23,17 @@ $disableCommonNames = [];
 foreach ($certList as $certEntry) {
     if ('R' === $certEntry['state']) {
         $commonName = sprintf('%s_%s', $certEntry['user_id'], $certEntry['name']);
-        $disableCommonNames[] = $commonName;
+        $disableCommonNames[] = [
+            'cn' => $commonName,
+            'exp' => $certEntry['exp'],
+        ];
     }
 }
 
 // create disable files for these CNs
 foreach ($disableCommonNames as $commonName) {
-    $commonNamePath = sprintf('%s/%s', $commonNamesDir, $commonName);
-    file_put_contents($commonNamePath, 'yes');
+    $disabledFile = sprintf('%s/%s', $disabledDir, $commonName['cn']);
+    // write the revoked time in the disabled file
+    echo $disabledFile.' ('.$commonName['exp'].')'.PHP_EOL;
+    file_put_contents($disabledFile, $commonName['exp']);
 }
