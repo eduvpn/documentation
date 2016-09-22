2cc5ba46e56b5d038486756167f48376937ce23e
François Kooman <fkooman@tuxed.net>
2016-09-22 09:37:45 +0200

    hopefully fix update_api_secrets

--- resources/update_api_secret.php
+++ resources/update_api_secret.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once '/usr/share/php/fkooman/Config/autoload.php';
 require_once '/usr/share/php/random_compat/autoload.php';
+require_once '/usr/share/php/Symfony/Component/Yaml/autoload.php';
 
-use fkooman\Config\YamlFile;
+use Symfony\Component\Yaml\Yaml;
 
 $vupvca = bin2hex(random_bytes(16));
 $vupvsa = bin2hex(random_bytes(16));
@@ -11,64 +11,73 @@ $vapvca = bin2hex(random_bytes(16));
 $vapvsa = bin2hex(random_bytes(16));
 $vsavca = bin2hex(random_bytes(16));
 
-#$vupvca = 'XXX-vpn-user-portal/vpn-ca-api-XXX';
-#$vupvsa = 'XXX-vpn-user-portal/vpn-server-api-XXX';
-#$vapvca = 'XXX-vpn-admin-portal/vpn-ca-api-XXX';
-#$vapvsa = 'XXX-vpn-admin-portal/vpn-server-api-XXX';
-#$vsavca = 'XXX-vpn-server-api/vpn-ca-api-XXX';
-
-$configFiles = [
-    'vpn-user-portal' => [
-        'config' => '/etc/vpn-user-portal/config.yaml',
-        'remoteApi' => [
-            'vpn-ca-api' => $vupvca,
-            'vpn-server-api' => $vupvsa,
-        ],
-    ],
-    'vpn-admin-portal' => [
-        'config' => '/etc/vpn-admin-portal/config.yaml',
-        'remoteApi' => [
-            'vpn-ca-api' => $vapvca,
-            'vpn-server-api' => $vapvsa,
-        ],
-    ],
-    'vpn-server-api' => [
-        'config' => '/etc/vpn-server-api/config.yaml',
-        'remoteApi' => [
-            'vpn-ca-api' => $vsavca,
-        ],
-        'api' => [
-            'vpn-user-portal' => $vupvsa,
-            'vpn-admin-portal' => $vapvsa,
-        ],
-    ],
-    'vpn-ca-api' => [
-        'config' => '/etc/vpn-ca-api/config.yaml',
-        'api' => [
-            'vpn-user-portal' => $vupvca,
-            'vpn-admin-portal' => $vapvca,
-            'vpn-server-api' => $vsavca,
-        ],
-    ],
-];
+//$vupvca = 'XXX-vpn-user-portal/vpn-ca-api-XXX';
+//$vupvsa = 'XXX-vpn-user-portal/vpn-server-api-XXX';
+//$vapvca = 'XXX-vpn-admin-portal/vpn-ca-api-XXX';
+//$vapvsa = 'XXX-vpn-admin-portal/vpn-server-api-XXX';
+//$vsavca = 'XXX-vpn-server-api/vpn-ca-api-XXX';
 
 try {
+    if (2 !== $argc) {
+        throw new RuntimeException('please specify the hostname');
+    }
+
+    $hostName = $argv[1];
+
+    $configFiles = [
+        'vpn-user-portal' => [
+            'config' => sprintf('/etc/vpn-user-portal/%s/config.yaml', $hostName),
+            'apiProviders' => [
+                'vpn-ca-api' => $vupvca,
+                'vpn-server-api' => $vupvsa,
+            ],
+        ],
+        'vpn-admin-portal' => [
+            'config' => sprintf('/etc/vpn-admin-portal/%s/config.yaml', $hostName),
+            'apiProviders' => [
+                'vpn-ca-api' => $vapvca,
+                'vpn-server-api' => $vapvsa,
+            ],
+        ],
+        'vpn-server-api' => [
+            'config' => sprintf('/etc/vpn-server-api/%s/config.yaml', $hostName),
+            'apiProviders' => [
+                'vpn-ca-api' => $vsavca,
+            ],
+            'apiConsumers' => [
+                'vpn-user-portal' => $vupvsa,
+                'vpn-admin-portal' => $vapvsa,
+            ],
+        ],
+        'vpn-ca-api' => [
+            'config' => sprintf('/etc/vpn-ca-api/%s/config.yaml', $hostName),
+            'apiConsumers' => [
+                'vpn-user-portal' => $vupvca,
+                'vpn-admin-portal' => $vapvca,
+                'vpn-server-api' => $vsavca,
+            ],
+        ],
+    ];
+
     foreach ($configFiles as $tokenId => $c) {
-        $yamlFile = new YamlFile($c['config']);
-        $configData = $yamlFile->readConfig();
-        if (array_key_exists('remoteApi', $c)) {
-            // remoteApi
-            foreach ($c['remoteApi'] as $k => $v) {
-                $configData['remoteApi'][$k]['token'] = $v;
+        $configFile = $c['config'];
+        $configData = Yaml::parse(file_get_contents($configFile));
+        if (array_key_exists('apiProviders', $c)) {
+            // apiProviders
+            foreach ($c['apiProviders'] as $k => $v) {
+                $configData['apiProviders'][$k]['userPass'] = $v;
             }
         }
-        if (array_key_exists('api', $c)) {
-            // api
-            foreach ($c['api'] as $k => $v) {
-                $configData['api'][$k]['token'] = $v;
+        if (array_key_exists('apiConsumers', $c)) {
+            // apiConsumers
+            foreach ($c['apiConsumers'] as $k => $v) {
+                $configData['apiConsumers'][$k] = $v;
             }
         }
-        $yamlFile->writeConfig($configData);
+
+        if (false === @file_put_contents($configFile, Yaml::dump($configData, 3))) {
+            throw new RuntimeException(sprintf('unable to write "%s"', $configFile));
+        }
     }
 } catch (Exception $e) {
     echo $e->getMessage().PHP_EOL;
