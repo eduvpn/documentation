94be76f3b4680e9257881946126487314bf4ed2b
François Kooman <fkooman@tuxed.net>
2016-12-18 16:48:28 +0100

    update dply.sh

--- resources/dply.sh
+++ resources/dply.sh
@@ -11,10 +11,10 @@ setsebool -P httpd_can_network_connect=1
 semanage port -a -t openvpn_port_t -p tcp 11940
 systemctl enable httpd
 sudo -u apache vpn-server-api-init
-sed -i 's/Require local/#Require local/' /etc/httpd.conf/vpn-user-portal.conf
-sed -i 's/Require local/#Require local/' /etc/httpd.conf/vpn-admin-portal.conf
-sed -i 's/#Require all granted/Require all granted/' /etc/httpd.conf/vpn-user-portal.conf
-sed -i 's/#Require all granted/Require all granted/' /etc/httpd.conf/vpn-admin-portal.conf
+sed -i 's/Require local/#Require local/' /etc/httpd/conf.d/vpn-user-portal.conf
+sed -i 's/Require local/#Require local/' /etc/httpd/conf.d/vpn-admin-portal.conf
+sed -i 's/#Require all granted/Require all granted/' /etc/httpd/conf.d/vpn-user-portal.conf
+sed -i 's/#Require all granted/Require all granted/' /etc/httpd/conf.d/vpn-admin-portal.conf
 vpn-user-portal-add-user --user foo --pass bar
 vpn-admin-portal-add-user --user foo --pass bar
 vpn-server-api-update-ip --profile internet --host "${EXTERNAL_IP}" --ext "${EXTERNAL_IF}"
@@ -25,3 +25,4 @@ systemctl start openvpn@default-internet-0
 vpn-server-node-generate-firewall --install
 systemctl enable iptables
 systemctl restart iptables
+echo '<html><head><title>Demo VPN</title></head><body><h1>Demo VPN</h1><ul><li><a href="vpn-user-portal/">User Portal</a></li><li><a href="vpn-admin-portal/">Admin Portal</a></ul><p>The username is <code>foo</code> and the password is <code>bar</code></p></body></html>' | tee /var/www/html/index.html > /dev/null
