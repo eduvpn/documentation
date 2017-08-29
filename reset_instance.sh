164dc5a47c43743e3cffe3652170c13adfcca8ed
François Kooman <fkooman@tuxed.net>
2017-08-29 23:47:53 +0200

    add more warnings

--- reset_instance.sh
+++ reset_instance.sh
@@ -2,6 +2,10 @@
 
 # you can use this script to "reset" a VPN instance, i.e. remove all data and 
 # reinitialize it to the state after fresh install using deploy.sh
+#
+# NOTE: *ALL* VPN configuration will stop working, all users need to obtain
+# a new configuration! All OAuth access tokens will become invalid!
+#
 
 INSTANCE=default
 
