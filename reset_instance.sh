f8aa601df2688bf9d76c756ecb5fda6ffbed4340
François Kooman <fkooman@tuxed.net>
2017-08-29 23:14:57 +0200

    add reset_instance.sh

---
+++ reset_instance.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# you can use this script to "reset" a VPN instance, i.e. remove all data and 
+# reinitialize it to the state after fresh install using deploy.sh
+
+INSTANCE=default
+
+systemctl stop php-fpm
+
+rm -rf /var/lib/vpn-server-api/${INSTANCE}
+rm -rf /var/lib/vpn-user-portal/${INSTANCE}
+rm -rf /var/lib/vpn-admin-portal/${INSTANCE}
+
+mkdir /var/lib/vpn-user-portal/${INSTANCE}
+chown apache.apache /var/lib/vpn-user-portal/${INSTANCE}
+chmod 0700 /var/lib/vpn-user-portal/${INSTANCE}
+sudo -u apache vpn-user-portal-init --instance ${INSTANCE}
+sudo -u apache vpn-server-api-init --instance ${INSTANCE}
+
+systemctl start php-fpm
+
+vpn-server-node-server-config --instance ${INSTANCE} --profile internet --generate
+systemctl restart "openvpn-server@*"
