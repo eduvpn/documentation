3a0226d5a3e35dfdc5fd51c5e5158ac89c311c5c
François Kooman <fkooman@tuxed.net>
2017-12-09 16:37:43 +0100

    add debian letsencrypt

---
+++ lets_encrypt_debian.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+#
+# Use Let's Encrypt to obtain certificates for the Web Server
+#
+
+###############################################################################
+# VARIABLES
+###############################################################################
+
+# DNS name of the Web Server
+printf "DNS name of the Web Server [vpn.example.org]: "; read -r WEB_FQDN
+WEB_FQDN=${WEB_FQDN:-vpn.example.org}
+
+###############################################################################
+# SYSTEM
+###############################################################################
+
+PACKAGE_MANAGER=/usr/bin/apt
+
+${PACKAGE_MANAGER} install -y certbot
+
+# stop Apache
+systemctl stop apache2
+
+###############################################################################
+# CERTBOT
+###############################################################################
+
+certbot certonly --standalone -d "${WEB_FQDN}"
+
+###############################################################################
+# APACHE
+###############################################################################
+
+sed -i "s|SSLCertificateFile /etc/ssl/certs/${WEB_FQDN}|#SSLCertificateFile /etc/ssl/certs/${WEB_FQDN}|" "/etc/apache2/sites-available/${WEB_FQDN}.conf"
+sed -i "s|SSLCertificateKeyFile /etc/ssl/private/${WEB_FQDN}.key|#SSLCertificateKeyFile /etc/ssl/private/${WEB_FQDN}.key|" "/etc/apache2/sites-available/${WEB_FQDN}.conf"
+
+sed -i "s|#SSLCertificateFile /etc/letsencrypt/live/${WEB_FQDN}/cert.pem|SSLCertificateFile /etc/letsencrypt/live/${WEB_FQDN}/cert.pem|" "/etc/apache2/sites-available/${WEB_FQDN}.conf"
+sed -i "s|#SSLCertificateKeyFile /etc/letsencrypt/live/${WEB_FQDN}/privkey.pem|SSLCertificateKeyFile /etc/letsencrypt/live/${WEB_FQDN}/privkey.pem|" "/etc/apache2/sites-available/${WEB_FQDN}.conf"
+sed -i "s|#SSLCertificateChainFile /etc/letsencrypt/live/${WEB_FQDN}/chain.pem|SSLCertificateChainFile /etc/letsencrypt/live/${WEB_FQDN}/chain.pem|" "/etc/apache2/sites-available/${WEB_FQDN}.conf"
+
+###############################################################################
+# CLEANUP
+###############################################################################
+
+# start Apache
+systemctl start apache2
+
+# ALL DONE!
