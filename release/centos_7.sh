1cc8c542cb1ff30827c8690e217a2a20efaa6e3b
François Kooman <fkooman@tuxed.net>
2018-06-18 12:13:44 +0200

    create symlink from 7Server to 7

--- release/centos_7.sh
+++ release/centos_7.sh
@@ -5,6 +5,13 @@ RPM_DIR=${REPO_ROOT}/release/enterprise/7/x86_64
 SRPM_DIR=${REPO_ROOT}/release/enterprise/7/Source
 MOCK_CONFIG=epel-7-x86_64
 
+(
+    # create a symlink from 7Server -> 7 for some? RHEL installations
+    mkdir -p ${REPO_ROOT}/release/enterprise/7
+    cd ${REPO_ROOT}/release/enterprise
+    ln -s 7 7Server
+)
+
 export REPO_ROOT RPM_DIR SRPM_DIR MOCK_CONFIG
 
 (
