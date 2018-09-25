71c52c6f27bf729e2ecd343e64b903450037abe5
François Kooman <fkooman@tuxed.net>
2018-09-25 17:55:26 +0200

    no longer require running update scripts with sudo, sudo is inside now

--- update_system_debian.sh
+++ update_system_debian.sh
@@ -4,28 +4,28 @@
 # *ONLY* run this during a maintenance window!
 #
 
-systemctl stop apache2
-systemctl stop php7.0-fpm
+sudo systemctl stop apache2
+sudo systemctl stop php7.0-fpm
 
 # install updates
-apt-get update && apt-get -y dist-upgrade 
+sudo apt-get update && sudo apt-get -y dist-upgrade 
 
 # delete template cache
 # ONLY enable when using your own custom theme not packaged as RPM/DEB
 #rm -rf /var/lib/vpn-user-portal/default/tpl
 #rm -rf /var/lib/vpn-admin-portal/default/tpl
 
-systemctl start php7.0-fpm
-systemctl start apache2
+sudo systemctl start php7.0-fpm
+sudo systemctl start apache2
 
 # regenerate OpenVPN config
-vpn-server-node-server-config
+sudo vpn-server-node-server-config
 
 # regenerate firewall
-vpn-server-node-generate-firewall --install
+sudo vpn-server-node-generate-firewall --install
 
 # restart firewall
-systemctl restart netfilter-persistent
+sudo systemctl restart netfilter-persistent
 
 # restart OpenVPN
-systemctl restart "openvpn-server@*"
+sudo systemctl restart "openvpn-server@*"
--- update_system_fedora.sh
+++ update_system_fedora.sh
@@ -4,29 +4,29 @@
 # *ONLY* run this during a maintenance window!
 #
 
-systemctl stop httpd
-systemctl stop php-fpm
+sudo systemctl stop httpd
+sudo systemctl stop php-fpm
 
 # install updates
-dnf -y --refresh update
+sudo dnf -y --refresh update
 
 # delete template cache
 # ONLY enable when using your own custom theme not packaged as RPM/DEB
 #rm -rf /var/lib/vpn-user-portal/default/tpl
 #rm -rf /var/lib/vpn-admin-portal/default/tpl
 
-systemctl start php-fpm
-systemctl start httpd
+sudo systemctl start php-fpm
+sudo systemctl start httpd
 
 # regenerate OpenVPN config
-vpn-server-node-server-config
+sudo vpn-server-node-server-config
 
 # regenerate firewall
-vpn-server-node-generate-firewall --install
+sudo vpn-server-node-generate-firewall --install
 
 # restart firewall
-systemctl restart iptables
-systemctl restart ip6tables
+sudo systemctl restart iptables
+sudo systemctl restart ip6tables
 
 # restart OpenVPN
-systemctl restart "openvpn-server@*"
+sudo systemctl restart "openvpn-server@*"
