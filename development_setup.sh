50befcb8246574fc80769a511281f635cc0d61b2
François Kooman <fkooman@tuxed.net>
2019-01-16 17:04:52 +0100

    restore development_setup.sh

--- development_setup.sh
+++ development_setup.sh
@@ -8,6 +8,7 @@ cd "${BASE_DIR}" || exit
 # clone all repositories
 git clone https://github.com/eduvpn/vpn-server-api.git
 git clone https://github.com/eduvpn/vpn-user-portal.git
+git clone https://github.com/eduvpn/vpn-admin-portal.git
 git clone https://github.com/eduvpn/vpn-server-node.git
 git clone https://github.com/eduvpn/vpn-lib-common.git
 
@@ -27,9 +28,19 @@ cp config/config.php.example config/default/config.php
 mkdir -p data/default
 php bin/init.php
 php bin/add-user.php --user foo --pass bar
-# XXX the secureCookie option is not there anymore in the default config 
-# template, deal with this differently!
 sed -i "s/'secureCookie' => true/'secureCookie' => false/" config/default/config.php
+sed -i "s/'enableTemplateCache' => true/'enableTemplateCache' => false/" config/default/config.php
+sed -i "s|'apiUri' => 'http://localhost/vpn-server-api/api.php'|'apiUri' => 'http://localhost:8008/api.php'|" config/default/config.php
+
+# vpn-admin-portal
+cd "${BASE_DIR}/vpn-admin-portal" || exit
+composer update
+mkdir config/default
+cp config/config.php.example config/default/config.php
+mkdir -p data/default
+php bin/add-user.php --user foo --pass bar
+sed -i "s/'secureCookie' => true/'secureCookie' => false/" config/default/config.php
+sed -i "s/'enableTemplateCache' => true/'enableTemplateCache' => false/" config/default/config.php
 sed -i "s|'apiUri' => 'http://localhost/vpn-server-api/api.php'|'apiUri' => 'http://localhost:8008/api.php'|" config/default/config.php
 
 # vpn-server-node
@@ -53,6 +64,11 @@ cat << 'EOF' | tee "${BASE_DIR}/launch.sh" > /dev/null
 (
     cd vpn-user-portal || exit
     VPN_INSTANCE_ID=default php -S localhost:8082 -t web &
+)
+
+(
+    cd vpn-admin-portal || exit
+    VPN_INSTANCE_ID=default php -S localhost:8083 -t web &
 )
 EOF
 chmod +x "${BASE_DIR}/launch.sh"
