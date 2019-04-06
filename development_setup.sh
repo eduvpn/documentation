8bbb509fe1ec166ca5fee8cd7c7f3f2d9b998479
François Kooman <fkooman@tuxed.net>
2019-04-06 11:44:50 +0200

    fix development_setup

--- development_setup.sh
+++ development_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-BASE_DIR=${HOME}/Projects/eduVPN
+BASE_DIR=${HOME}/Projects/tmp/eduVPN
 
 mkdir -p "${BASE_DIR}"
 cd "${BASE_DIR}" || exit
@@ -13,34 +13,45 @@ git clone https://github.com/eduvpn/vpn-lib-common.git
 
 # vpn-server-api
 cd "${BASE_DIR}/vpn-server-api" || exit
-composer update
-mkdir config
-cp config/config.php.example config/config.php
 mkdir -p data
+composer update
+cp config/config.php.example config/config.php
 php bin/init.php
 
 # vpn-user-portal
 cd "${BASE_DIR}/vpn-user-portal" || exit
-composer update
-mkdir config
-cp config/config.php.example config/config.php
 mkdir -p data
+composer update
+
+cat << 'EOF' > config/config.php
+<?php
+$baseConfig = include __DIR__.'/config.php.example';
+$localConfig = [
+    'secureCookie' => false,
+    'apiUri' => 'http://localhost:8008/api.php',
+];
+return array_merge($baseConfig, $localConfig);
+EOF
+
 php bin/init.php
-php bin/add-user.php --user foo --pass bar
-# XXX the secureCookie option is not there anymore in the default config 
-# template, deal with this differently!
-sed -i "s/'secureCookie' => true/'secureCookie' => false/" config/config.php
-sed -i "s|'apiUri' => 'http://localhost/vpn-server-api/api.php'|'apiUri' => 'http://localhost:8008/api.php'|" config/config.php
+php bin/generate-oauth-key.php
+php bin/add-user.php --user foo   --pass bar
+php bin/add-user.php --user admin --pass secret
 
 # vpn-server-node
 cd "${BASE_DIR}/vpn-server-node" || exit
+mkdir -p data openvpn-config
 composer update
-mkdir config
-cp config/config.php.example config/config.php
+
 cp config/firewall.php.example config/firewall.php
-mkdir -p data
-mkdir openvpn-config
-sed -i "s|'apiUri' => 'http://localhost/vpn-server-api/api.php'|'apiUri' => 'http://localhost:8008/api.php'|" config/config.php
+cat << 'EOF' > config/config.php
+<?php
+$baseConfig = include __DIR__.'/config.php.example';
+$localConfig = [
+    'apiUri' => 'http://localhost:8008/api.php',
+];
+return array_merge($baseConfig, $localConfig);
+EOF
 
 # launch script
 cat << 'EOF' | tee "${BASE_DIR}/launch.sh" > /dev/null
