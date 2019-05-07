e6f00463c7d6e3401ebf015a69dd245745790a27
François Kooman <fkooman@tuxed.net>
2019-05-08 00:24:26 +0200

    add F30 release script

---
+++ release/fedora_30_x86_64.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+OS_VERSION=30
+MOCK_CONFIG=fedora-${OS_VERSION}-x86_64
+REPO_ROOT=${HOME}/repo-v2/${MOCK_CONFIG}
+RPM_DIR=${REPO_ROOT}/release/fedora/${OS_VERSION}/x86_64
+SRPM_DIR=${REPO_ROOT}/release/fedora/${OS_VERSION}/Source
+
+export REPO_ROOT RPM_DIR SRPM_DIR MOCK_CONFIG
+
+(
+    sh release/build_packages.sh
+)
