a9ab859c09f1119c51c6535a9d30c88f1902135e
François Kooman <fkooman@tuxed.net>
2019-05-08 23:10:17 +0200

    update v2 development_setup script

--- development_setup.sh
+++ development_setup.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
-BASE_DIR=${HOME}/Projects/LC
+LC_BRANCH=v2
+BASE_DIR=${HOME}/Projects/LC-${LC_BRANCH}
 
 mkdir -p "${BASE_DIR}"
 cd "${BASE_DIR}" || exit
 
 # clone repositories (read-only)
-git clone https://github.com/eduvpn/vpn-user-portal.git
-git clone https://github.com/eduvpn/vpn-server-node.git
+git clone -b ${LC_BRANCH} https://github.com/eduvpn/vpn-lib-common.git
+git clone -b ${LC_BRANCH} https://github.com/eduvpn/vpn-user-portal.git
+git clone -b ${LC_BRANCH} https://github.com/eduvpn/vpn-server-api.git
+git clone -b ${LC_BRANCH} https://github.com/eduvpn/vpn-server-node.git
 
 # clone all repositories (read/write, your own "fork")
 #GITHUB_USER=fkooman
-#git clone git@github.com:${GITHUB_USER}/vpn-user-portal.git
-#git clone git@github.com:${GITHUB_USER}/vpn-server-node.git
+#git clone -b ${LC_BRANCH} git@github.com:${GITHUB_USER}/vpn-lib-common.git
+#git clone -b ${LC_BRANCH} git@github.com:${GITHUB_USER}/vpn-user-portal.git
+#git clone -b ${LC_BRANCH} git@github.com:${GITHUB_USER}/vpn-server-api.git
+#git clone -b ${LC_BRANCH} git@github.com:${GITHUB_USER}/vpn-server-node.git
 
 # vpn-user-portal
 cd "${BASE_DIR}/vpn-user-portal" || exit
@@ -34,6 +39,21 @@ php bin/generate-oauth-key.php
 php bin/add-user.php --user foo   --pass bar
 php bin/add-user.php --user admin --pass secret
 
+# vpn-server-api
+cd "${BASE_DIR}/vpn-server-api" || exit
+mkdir -p data
+composer update
+
+cat << 'EOF' > config/config.php
+<?php
+$baseConfig = include __DIR__.'/config.php.example';
+$localConfig = [
+];
+return array_merge($baseConfig, $localConfig);
+EOF
+
+php bin/init.php
+
 # vpn-server-node
 cd "${BASE_DIR}/vpn-server-node" || exit
 mkdir -p data openvpn-config
@@ -51,6 +71,10 @@ EOF
 # launch script
 cat << 'EOF' | tee "${BASE_DIR}/launch.sh" > /dev/null
 #!/bin/sh
+(
+    cd vpn-server-api || exit
+    php -S localhost:8008 -t web &
+)
 (
     cd vpn-user-portal || exit
     php -S localhost:8082 -t web &
