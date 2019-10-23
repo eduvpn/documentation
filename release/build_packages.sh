42d772009977554bf9109bfa2c9a17d6d9402634
François Kooman <fkooman@tuxed.net>
2019-10-23 16:00:29 +0200

    update build script

--- release/build_packages.sh
+++ release/build_packages.sh
@@ -28,13 +28,26 @@ cp ./*.pub ./*.conf ./*.cron ./*.patch ./gpgkey-* "${HOME}/rpmbuild/SOURCES"
 for f in "${PACKAGE_LIST[@]}"
 do
     spectool -g -R "${f}".spec || exit 1
-    SRPM_FILE=$(rpmbuild -bs "${f}".spec | cut -d ':' -f 2)
-    SRPM_LIST+=" ${SRPM_FILE}"
+    SRPM_FILE=$(rpmbuild -bs "${f}".spec | grep Wrote | cut -d ':' -f 2 | xargs)
+	PACKAGE_NAME=$(basename "${SRPM_FILE}" .src.rpm)
+	if [ ! -f "${REPO_ROOT}/results/${MOCK_CONFIG}/${PACKAGE_NAME}/success" ]
+	then
+		# only list the package for building if this exact version did not 
+		# build successfully before...
+		SRPM_LIST="${SRPM_LIST} ${SRPM_FILE}"
+	fi
 done
+
+# only call mock when we have something to do...
+if [ "" = "${SRPM_LIST}" ]
+then
+        echo "*** No (new) packages to build!"
+        exit 0
+fi
 
 echo "Build in progress, this may take a long time..."
 (
-    mock --chain -r "${MOCK_CONFIG}" --localrepo="${REPO_ROOT}" --arch "${ARCH}" ${SRPM_LIST} 
+    mock --chain -r "${MOCK_CONFIG}" --localrepo="${REPO_ROOT}" --arch "${ARCH}" "${SRPM_LIST}" 
 )
 
 (
