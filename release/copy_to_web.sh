83260a60d356f9e935a397578a1c33fe231f109b
François Kooman <fkooman@tuxed.net>
2019-10-25 15:27:44 +0200

    remove all rpm/rpm-dev and release scripts

--- release/copy_to_web.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-REPO_ROOT=${HOME}/repo-v2
-WEB_ROOT=/var/www/html
-
-mkdir -p ${WEB_ROOT}/repo
-cp ${HOME}/RPM-GPG-KEY-LC ${WEB_ROOT}/repo/
-cp -r ${REPO_ROOT}/results/* ${WEB_ROOT}/repo/
-restorecon -R ${WEB_ROOT}/repo
--- rpm-dev/build_all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-for i in *.spec
-do
-	BN=$(basename $i .spec)
-	./local_build.sh ${BN} && ./copr_build.sh ${BN} &
-done
