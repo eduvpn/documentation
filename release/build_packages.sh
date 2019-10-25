86a234788a6345111a5a5a69b9056a2af47bd80b
François Kooman <fkooman@tuxed.net>
2019-10-25 13:06:11 +0200

    rewrite build script again

--- release/build_packages.sh
+++ release/build_packages.sh
@@ -1,41 +1,58 @@
 #!/bin/bash
 
-REPO_ROOT=${HOME}/repo-v2
-PACKAGE_BRANCH=master
+PACKAGE_BRANCH=v2
+#PACKAGE_BRANCH=master
+REPO_ROOT=${HOME}/repo-${PACKAGE_BRANCH}
 
 # targets to build for
 TARGET_LIST=(\
     epel-7-x86_64 \
     fedora-30-x86_64 \
-    #fedora-30-aarch64 \
-    fedora-31-x86_64 \
-    #fedora-31-aarch64 \
+    fedora-31-x86_64 \ 
+)
+	
+# list of packages to build, *in this order*
+PACKAGE_LIST=(\
+    php-fkooman-saml-sp \
+    php-fkooman-jwt \
+    php-fkooman-otp-verifier \
+    php-fkooman-oauth2-server \
+    php-fkooman-secookie \
+    php-fkooman-sqlite-migrate \
+    php-LC-openvpn-connection-manager \
+    php-LC-common \
+    vpn-server-api \
+    vpn-server-node \
+    vpn-user-portal \
+    vpn-portal-artwork-eduVPN \
+    vpn-portal-artwork-LC \
 )
 
+# update the repositories with RPM spec files
+for PACKAGE_NAME in "${PACKAGE_LIST[@]}"
+do	
+    # if we already have the package git repository, simply update it
+    if [ ! -f "${PACKAGE_NAME}" ]
+    then
+        (
+            cd ${PACKAGE_NAME}
+            git checkout ${PACKAGE_BRANCH}
+            git pull origin ${PACKAGE_BRANCH}
+        )
+    else
+		# otherwise, clone it
+        git clone -b ${PACKAGE_BRANCH} https://git.tuxed.net/rpm/${PACKAGE_NAME}
+    fi
+done
+	
 for TARGET_NAME in "${TARGET_LIST[@]}"
 do
-	echo "**********************************************************"
-	echo "* Building ${TARGET_NAME}..."
-	echo "**********************************************************"
+    echo "**********************************************************"
+    echo "* Building ${TARGET_NAME}..."
+    echo "**********************************************************"
+
     # determine ARCH based on target name
     ARCH=$(echo ${TARGET_NAME} | cut -d '-' -f 3)
-
-    # list of packages to build, *in this order*
-    PACKAGE_LIST=(\
-        php-fkooman-saml-sp \
-        php-fkooman-jwt \
-        php-fkooman-otp-verifier \
-        php-fkooman-oauth2-server \
-        php-fkooman-secookie \
-        php-fkooman-sqlite-migrate \
-        php-LC-openvpn-connection-manager \
-        php-LC-common \
-        vpn-server-api \
-        vpn-server-node \
-        vpn-user-portal \
-        vpn-portal-artwork-eduVPN \
-        vpn-portal-artwork-LC \
-    )
 
     SRPM_LIST=""
 
@@ -43,27 +60,14 @@ do
     # packages that need to be (re)build
     for PACKAGE_NAME in "${PACKAGE_LIST[@]}"
     do
-		# if we already have the package git repository, simply update it
-		if [ ! -f "${PACKAGE_NAME}" ]
-		then
-			(
-				cd ${PACKAGE_NAME}
-				git checkout ${PACKAGE_BRANCH}
-				git pull origin ${PACKAGE_BRANCH}
-			)
-		else
-			# clone fresh
-      		git clone -b ${PACKAGE_BRANCH} https://git.tuxed.net/rpm/${PACKAGE_NAME}
-		fi
-		
-		# generate source RPM
+        # generate source RPM
         cp ${PACKAGE_NAME}/SOURCES/* ${HOME}/rpmbuild/SOURCES
-		spectool -g -R ${PACKAGE_NAME}/SPECS/${PACKAGE_NAME}.spec
-		SRPM_FILE=$(rpmbuild -bs "${PACKAGE_NAME}/SPECS/${PACKAGE_NAME}".spec | grep Wrote | cut -d ':' -f 2 | xargs)
+        spectool -g -R ${PACKAGE_NAME}/SPECS/${PACKAGE_NAME}.spec
+        SRPM_FILE=$(rpmbuild -bs "${PACKAGE_NAME}/SPECS/${PACKAGE_NAME}".spec | grep Wrote | cut -d ':' -f 2 | xargs)
         PACKAGE_NAME=$(basename "${SRPM_FILE}" .src.rpm)
 
-		# check whether we already have a build of this exact version, if not,
-		# add it to the list
+        # check whether we already have a build of this exact version, if not,
+        # add it to the list
         if [ ! -f "${REPO_ROOT}/results/${TARGET_NAME}/${PACKAGE_NAME}/success" ]
         then
             SRPM_LIST="${SRPM_LIST} ${SRPM_FILE}"
@@ -73,12 +77,13 @@ do
     # only call mock when we have something to do...
     if [ "" = "${SRPM_LIST}" ]
     then
-            echo "*** No (new) packages to build!"
-			continue
-	fi
+        echo "*** No (new) packages to build!"
+		# continue with the next TARGET
+        continue
+    fi
 
-	echo "**********************************************************"
-	echo "* Building (${TARGET_NAME}): ${SRPM_LIST}..."
-	echo "**********************************************************"
+    echo "**********************************************************"
+    echo "* Building (${TARGET_NAME}): ${SRPM_LIST}..."
+    echo "**********************************************************"
     mock --chain -r "${TARGET_NAME}" --localrepo="${REPO_ROOT}" --arch "${ARCH}" ${SRPM_LIST}
 done
